4015c053bd96d082c640266bee08e27157534d8e
Trygve Aaberge <trygveaa@gmail.com>
2014-06-22 22:20:45 +0200

    Setup initial project

---
+++ setup.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+
+import re
+from setuptools import setup, find_packages
+
+
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
+
+
+setup(
+    name='Python-Tunigo',
+    version=get_version('tunigo/__init__.py'),
+    url='https://github.com/trygveaa/python-tunigo',
+    license='Apache License, Version 2.0',
+    author='Trygve Aaberge',
+    author_email='trygveaa@gmail.com',
+    description='Python API for the browse feature of Spotify',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock >= 1.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
---
+++ tunigo/api.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+
+class Tunigo(object):
+
+    def __init__(self):
+        noop
