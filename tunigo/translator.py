1847414878fd0987737b2643778069739d12bb23
Trygve Aaberge <trygveaa@gmail.com>
2014-06-23 18:13:08 +0200

    Cache results from underscore_to_camelcase
    This method is called very many times, with a limited set of values. Add
    a cache to not do lots of unneeded operations.

--- tunigo/translator.py
+++ tunigo/translator.py
@@ -1,6 +1,14 @@
 from __future__ import unicode_literals
 
 
+underscore_to_camelcase_cache = {}
+
+
 def underscore_to_camelcase(word):
-    s = ''.join(part.capitalize() for part in word.split('_'))
-    return s[0].lower() + s[1:]
+    if word in underscore_to_camelcase_cache:
+        return underscore_to_camelcase_cache[word]
+    else:
+        s = ''.join(part.capitalize() for part in word.split('_'))
+        s = s[0].lower() + s[1:]
+        underscore_to_camelcase_cache[word] = s
+        return s
