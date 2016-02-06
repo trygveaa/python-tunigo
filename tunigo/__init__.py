e0193d1063adba66462fbb7442498207076e2f2b
Trygve Aaberge <trygveaa@gmail.com>
2016-02-06 12:16:00 +0100

    Move __version__ below imports
    This fixes a flake error.

--- tunigo/__init__.py
+++ tunigo/__init__.py
@@ -1,13 +1,13 @@
 from __future__ import unicode_literals
 
 
-__version__ = '0.1.3'
-
-
 from tunigo.api import Tunigo
 from tunigo.genre import Genre, SubGenre
 from tunigo.playlist import Playlist
 from tunigo.release import Release
+
+
+__version__ = '0.1.3'
 
 
 __all__ = [
